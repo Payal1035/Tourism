bbcd9b13ff7762d0d56e7c65e5b73d5354027b0a
Payal kumari <87144905+Payal1035@users.noreply.github.com>
2022-05-11 16:28:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var video = document.getElementById("myVideo");
+
+const demo=document.getElementById('reg');
+const but=document.querySelector('.btn1');
+const img1=document.querySelector('.t2');
+
+but.addEventListener('click',show);
+
+function show(){
+    demo.style.display='block';
+}
+
